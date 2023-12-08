01555693b2591aba6eb4c432205e67c7428183fb
Azzah Azkiyah <azzah.azkiyah@ui.ac.id>
2023-12-08 20:40:48 +0700

    Landing Page Fixed

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,55 +1,58 @@
-const mongoose = require('mongoose');
+import React from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useNavigation } from '@react-navigation/native';
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ['UMKM', 'STUDENT'],
-    required: true,
-  },
-  // Fields specific to UMKM
-  annualTurnover: {
-    type: Number,
-    default: null,
-  },
-  businessLifespan: {
-    type: Number,
-    default: null,
-  },
-  businessIndustry: {
-    type: String,
-    default: null,
-  },
-  // Fields specific to STUDENT
-  major: {
-    type: String,
-    default: null,
-  },
-  semester: {
-    type: String,
-    default: null,
-  },
-  skillLevel: {
-    type: String,
-    enum: ['Amateur', 'Junior', 'Pro'],
-    default: null,
-  },
-  // Other user profile fields
-}, { timestamps: true });
+const LandingScreen = () => {
+  const navigation = useNavigation();
 
-const User = mongoose.model('User', userSchema);
+  const handleRoleSelection = (role) => {
+    navigation.navigate('Register'); // Navigate to RegisterScreen
+    console.log('Selected role:', role);
+  };
 
-module.exports = User;
+
+  return (
+    <LinearGradient
+      colors={['#FEFEFE', '#5093A6']}
+      style={{ flex: 1 }}
+    >
+      <View className="flex-1 justify-center items-center">
+        <Image
+          source={require('../assets/login_img.png')}
+          className="mb-8"
+        />
+        <View className="w-11/12 bg-white rounded-[20px] shadow-md p-8">
+          <Text className="text-2xl font-bold mb-6 text-center text-black">Seize The Opportunity </Text>
+          <Text className="mb-6 text-center text-[#8F959E]">Empowering Tomorrow's Innovators, One Connection at a Time </Text>
+          <View className="flex-row justify-between mb-4">
+            <TouchableOpacity
+              onPress={() => handleRoleSelection('UMKM')}
+              className="flex-1 bg-[#F5F6FA] py-3 rounded-md mr-2"
+            >
+              <Text 
+              style={{ color: '#8F959E' }} 
+              className="text-center">UMKM</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleRoleSelection('STUDENT')}
+              className="flex-1 bg-[#5093A6] py-3 rounded-md ml-2"
+            >
+              <Text className="text-center text-white">STUDENT</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            onPress={() => handleRoleSelection('SKIP')}
+            className="bg-gray-200 py-3 rounded-md"
+          >
+            <Text 
+            style={{ color: '#8F959E' }} 
+            className="text-center">SKIP</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </LinearGradient>
+  );
+};
+
+export default LandingScreen;
